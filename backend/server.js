f104e8a426ec2b01d1b67c2cbf4986e93f26c027
Nikita Bawa <nikitabawa@nikitas-mbp.attlocal.net>
2020-07-23 18:31:34 -0400

    trying to get git to pick up the change

--- backend/server.js
+++ backend/server.js
@@ -15,10 +15,13 @@ const answersRouter = require('./routes/answers');
 app.use('/api/topics', topicsRouter);
 app.use('/api/answers', answersRouter);
 
-app.use(express.static(path.join(__dirname, '../build')))
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../build'))
-});
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html')); // relative path
+    });
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
